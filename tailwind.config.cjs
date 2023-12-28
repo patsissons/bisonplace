b12df52df684809526e001408ff3d6cb583dbdd9
Pat Sissons <patricksissons@gmail.com>
2023-12-28 14:34:48 -0800

    migrating to svelte

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      screens: {
+        xs: '420px',
+      },
+      fontFamily: {
+        orbitron: ['Orbitron', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        bison: {
+          bg: '#838484',
+          fg: '#d1d2d4',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
