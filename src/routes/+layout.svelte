0fa6205343d2128e6a7d0ae408afee5e33ca683b
Pat Sissons <patricksissons@gmail.com>
2023-12-28 17:24:23 -0800

    adding menus

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+  import { page } from '$app/stores'
   import '$lib/styles/app.css'
+
+  // this is broken for some reason
+  // see: https://github.com/sveltejs/eslint-plugin-svelte/issues/652
+  // eslint-disable-next-line svelte/valid-compile
+  $: isMenusPage = $page.route.id === '/menus'
 </script>
 
 <div
-  class="flex flex-col gap-2 min-h-screen bg-[radial-gradient(circle_at_center,var(--tw-gradient-stops))] from-bison-neon-bg/50 from-25% to-bison-neon-fg/50"
+  class="flex flex-col gap-2 min-h-screen max-h-screen bg-[radial-gradient(circle_at_center,var(--tw-gradient-stops))] from-bison-neon-bg/50 from-25% to-bison-neon-fg/50"
 >
   <header class="text-center px-2 py-0.5">
     <h1
@@ -13,9 +19,20 @@
     </h1>
   </header>
 
-  <main class="flex-1 flex">
+  <main class="flex-1 flex overflow-clip">
     <slot />
   </main>
 
-  <footer class="text-center px-2 py-0.5" />
+  <footer class="text-center px-2 py-0.5">
+    <a
+      class="text-bison-neon-bg text-xl font-orbitron"
+      href={isMenusPage ? '/' : '/menus'}
+    >
+      {#if isMenusPage}
+        Home
+      {:else}
+        Menus
+      {/if}
+    </a>
+  </footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import Bison from './bison.svelte'
 </script>
 
-<section class="flex items-center justify-center w-full">
+<section class="flex-1 flex items-center justify-center w-full">
   <div class="h-full">
     <Bison
       class="h-full max-w-full min-w-[320px]"
