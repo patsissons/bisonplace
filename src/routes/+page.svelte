acc8f49385bac4ce5a60617308ccf45f342c0447
Pat Sissons <patricksissons@gmail.com>
2024-12-29 14:00:49 -0800

    simplify bison logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,6 @@
 
 <section class="flex-1 flex items-center justify-center w-full">
   <div class="h-full">
-    <Bison
-      class="h-full max-w-full min-w-[320px]"
-      fg="#EEE9DA"
-      bg="#6096B4"
-      horns="#EEE9DA"
-    />
+    <Bison class="h-full max-w-full min-w-[320px]" />
   </div>
 </section>
